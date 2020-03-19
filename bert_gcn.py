c158f86e44fb84514fa9b8fa16616c511a479b61
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-19 09:38:03 +0800

    init

--- bert_gcn.py
+++ bert_gcn.py
@@ -179,6 +179,8 @@ class BertGCNClassifier():
         total_run_time = 0.0
 
         param_optimizer = list(self.model.named_parameters())
+        print(param_optimizer)
+        exit()
         no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
         optimizer_grouped_parameters = [
             {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
